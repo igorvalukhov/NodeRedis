14629898a6f24d93eff2fd0930d15bc89a532263
IgorV <igor.valukhov@gmail.com>
2019-10-31 10:35:56 +0300

    added redis cache

--- index.js
+++ index.js
@@ -5,25 +5,49 @@ const redis = require('redis');
 const PORT = process.env.PORT || 5000;
 const REDIS_PORT = process.env.PORT || 6379;
 
-const client = redis.createClient(REDIS_PORT);
+const redisClient = redis.createClient(REDIS_PORT);
 
 const app = express();
 
-//make request from GitHub for <datalist>
+//Set response
+function setResponse(username, repos) {
+    return `<h2>${username} has ${repos} GitHub repos</h2>`;
+}
+
+//Make request from GitHub for <datalist>
 async function getRepos(req, res, next) {
     try {
         console.log('Fetching Data...');
-        const {username} = req.params;
+        const { username } = req.params;
         const response = await fetch(`https://api.github.com/users/${username}`);
         const data = await response.json();
-        res.send(data);
+        const repos = data.public_repos;
+
+        //set data to redis
+        redisClient.setex(username, 3600, repos);
+
+        res.send(setResponse(username, repos));
     } catch(err) {
         console.error(err);
         res.status(500);
     }
 }
 
-app.get('/repos/:username', getRepos);
+//Cache middleware
+function cache(req, res, next) {
+    const { username } = req.params;
+    redisClient.get(username, (err, data) => {
+        if(err) throw err;
+
+        if(data !== null) {
+            res.send(setResponse(username, data)); 
+        } else {
+            next();
+        }
+    });
+}
+
+app.get('/repos/:username', cache, getRepos);
 
 app.listen(5000, () => {
     console.log(`App listening on port ${PORT}`);
